c63a6ae6f16addb28d2c2013402e72b7a28e26a4
fabuzard <44804900+fabuzard@users.noreply.github.com>
2023-03-13 20:40:18 +0700

    home

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { Routes, Route } from 'react-router-dom'
+import LearnHOC from './pages/LearnHOC';
+import Testing from './pages/Testing';
+import Home from './pages/Home';
+
+function App() {
+  // const Button = (props) => <button style={props.style}>Click me</button>
+  // const Text = (props) => <p style={props.style}>Testing</p>
+
+  // const StyleButton = LearnHOC(Button)
+  // const StyleText = LearnHOC(Text)
+
+  return (
+    <div>
+      {/* <StyleButton text="test" />
+      <Button />
+      <StyleText /> */}
+      <Routes>
+
+        <Route path='/' element={<Home />} />
+        <Route path='/testing' element={<Testing />} />
+        <Route path='/learn' element={<LearnHOC />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
